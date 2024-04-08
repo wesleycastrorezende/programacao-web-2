d2510d39dff8f27bce4a8a6bc9d933c7f102c8c1
wesleycastrorezende <120793429+wesleycastrorezende@users.noreply.github.com>
2024-04-08 19:30:10 -0400

    aula 2

---
+++ aula 2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="aula 2/resultado.php">
+    <label for="nome"> Nome </label>
+    <input type="text" id = "nome" name = "nome">
+    <label for="idade"> idade</label>
+    <input type="number" id="idade" name="idade">
+    <button type="submit"> Enviar</button>
+    </form>
+</body>
+</html>
---
+++ aula 2/resultado.php
@@ -0,0 +1,7 @@
+<?php
+
+$nome = $_GET["nome"];
+$idade = $_GET["idade"];
+echo "<p> Ola! $nome sua idade é $idade</p>";
+print "<a href='index.php'> voltar </a>";
+?>
