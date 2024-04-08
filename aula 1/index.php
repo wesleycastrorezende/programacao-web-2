caa4a0699e533aad08dda36934b66621ead4e766
wesleycastrorezende <120793429+wesleycastrorezende@users.noreply.github.com>
2024-04-08 19:29:55 -0400

    aula 1

---
+++ aula 1/index.php
@@ -0,0 +1,20 @@
+<p>Qualquer texto</p>
+<?php
+// tag de abertura 
+$nome = "wesley";
+echo   "O nome é $nome";
+// tag de fechamento
+?>
+
+<h2>Estrutura de condicinal</h2>
+<?php
+$altura = 1.70;
+if ($altura < 1.60){
+    echo "autorizado a entrar";
+
+}
+else
+{
+    echo "nao autorizado";
+}
+?>
